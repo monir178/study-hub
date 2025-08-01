822376b647e84cacbb08a27ca11ad9d40b005f2f
Md Moniruzzaman Monir <monir.mzs17@gmail.com>
2025-08-02 05:30:57 +0600

    refactor: remove useSocket hook and update exports
    - Deleted the useSocket hook to streamline the hooks library.
    - Updated index.ts to reflect the removal of useSocket, ensuring all exports are current.

--- lib/hooks/index.ts
+++ lib/hooks/index.ts
@@ -1,6 +1,5 @@
 // Re-export all custom hooks
 export { useAuth } from "./useAuth";
-export { useSocket } from "./useSocket";
 export { useLocalStorage } from "./useLocalStorage";
 export { useTimer } from "./useTimer";
 export { useDebounce } from "./useDebounce";
